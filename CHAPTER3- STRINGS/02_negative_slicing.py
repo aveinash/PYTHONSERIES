6a3c4a67a39db617b2a8d970b208e125ef719926
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-02 10:05:27 +0530

    commit

---
+++ CHAPTER3- STRINGS/02_negative_slicing.py
@@ -0,0 +1,10 @@
+name = "avinash"
+
+print(name[0:3]) # print from index 0 to 3 excluding 3
+
+print(name[-4:-1]) # sirf corresponding krna h 
+print(name[1:4])
+
+print(name[:4]) # is same as print print(name[0:4]) 
+print(name[1:]) # is same as print print(name[1:5]) 5 mtlb total length
+
