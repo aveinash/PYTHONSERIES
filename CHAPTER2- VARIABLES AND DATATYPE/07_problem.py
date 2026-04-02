0bb630e40b3665c9a0a72376ee20869f9914a59f
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-02 09:33:58 +0530

    added

---
+++ CHAPTER2- VARIABLES AND DATATYPE/07_problem.py
@@ -0,0 +1,9 @@
+# Use comparison operator to find out wheter a given variable a is greater than b or not 
+
+a = int(input("Enter number 1: "))
+
+b = int(input("Enter number 2: "))
+
+print("a is grater than b is",a>b)
+
+
