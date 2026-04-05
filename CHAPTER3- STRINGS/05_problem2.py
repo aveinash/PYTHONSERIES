5bed09e7e85209c2b62960d9ea3fc3c4d5944d5d
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-05 15:30:29 +0530

    add

---
+++ CHAPTER3- STRINGS/05_problem2.py
@@ -0,0 +1,5 @@
+# write a program to detect double space in a string 
+# name.find is the main thing
+
+name = "today is a good  day"
+print(name.find("  "))
---
+++ CHAPTER3- STRINGS/06_problem3.py
@@ -0,0 +1,3 @@
+# replaces the double spaces from problem 5 with single spaces
+name = "today is a good  day"
+print(name.replace("  "," "))
