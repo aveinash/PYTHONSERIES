0c2be3d588fe53292e97583875de96f649d95fc0
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-05 16:19:08 +0530

    add

---
+++ CHAPTER4- LISTS AND TUPLES/02_lists_methods.py
@@ -0,0 +1,14 @@
+random = ["apple", 4565.3, "rohan", False, "avinash"] 
+random.append("guava") # insertion hua guava 
+print(random)
+
+# sort method automatically sort krdega increading order me 
+l1 = [3,2,4,1,4,2]
+l1.sort()
+print(l1)
+
+l1.reverse() # reverse krne k liye
+print(l1)
+
+l1.insert(2,13424) # kahi beech me insert krne k liye we use insert
+print(l1)
