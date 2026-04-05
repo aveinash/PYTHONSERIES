85852791fc8bc404ad4fe19e4031622e4c8b3433
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-05 15:32:15 +0530

    add

--- CHAPTER3- STRINGS/06_problem3.py
+++ CHAPTER3- STRINGS/06_problem3.py
@@ -1,3 +1,4 @@
 # replaces the double spaces from problem 5 with single spaces
 name = "today is a good  day"
 print(name.replace("  "," "))
+print(name) # ye string change nahi hui hh upar usne khud ek new string banai h 
