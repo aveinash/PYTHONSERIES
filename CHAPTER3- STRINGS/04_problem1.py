bc6b07cb9cd52ff7867d6e1f10119f56a4cb9a91
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-05 15:14:50 +0530

    commit

---
+++ CHAPTER3- STRINGS/04_problem1.py
@@ -0,0 +1,5 @@
+# write a python program to display a user entered name followed by good afternoon using input()
+
+a = input("enter your name: ")
+print("good afternoon ",a)
+print(f"good morning {a}") #f string 
