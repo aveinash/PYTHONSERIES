540178a5592c6f69504beaac5c6e0c60ff57aa77
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-05 15:33:55 +0530

    a

--- CHAPTER3- STRINGS/06_problem3.py
+++ CHAPTER3- STRINGS/06_problem3.py
@@ -2,3 +2,4 @@
 name = "today is a good  day"
 print(name.replace("  "," "))
 print(name) # ye string change nahi hui hh upar usne khud ek new string banai h 
+# thats why string is immutable which means that you cannot change them by running functions on them
