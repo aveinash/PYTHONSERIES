847fc5d8d1645f596bbe966293007b1497f49638
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-12 16:22:54 +0530

    a

---
+++ CHAPTER4- LISTS AND TUPLES/04_problem.py
@@ -0,0 +1,14 @@
+# write a program to store five fruits in a list entered by user 
+
+fruits = []
+f1 = input("enter fruit name 1: ")
+fruits.append(f1)
+f2 = input("enter fruit name 2: ")
+fruits.append(f2)
+f3 = input("enter fruit name 3: ")
+fruits.append(f3)
+f4 = input("enter fruit name 4: ")
+fruits.append(f4)
+f5 = input("enter fruit name 5: ")
+fruits.append(f5)
+print(fruits)
