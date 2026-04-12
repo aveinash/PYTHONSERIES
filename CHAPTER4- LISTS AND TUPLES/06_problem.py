97637e2794cf25f072e4aad795a9a732e34f6c7d
aveinash <avinashthakur.ytcreator@gmail.com>
2026-04-12 16:51:45 +0530

    better message

---
+++ CHAPTER4- LISTS AND TUPLES/06_problem.py
@@ -0,0 +1,5 @@
+# write a program to sum a list with 4 numbers 
+
+l1 = [1,3,2,4]
+
+print(sum(l1))
---
+++ CHAPTER4- LISTS AND TUPLES/07_problem.py
@@ -0,0 +1,5 @@
+# write a program to count the number of zeros in the following tuple 
+# a = (4,0,4,2,0,4,0,1) 
+
+a = (4,0,4,2,0,4,0,1) 
+print(a.count(0))
