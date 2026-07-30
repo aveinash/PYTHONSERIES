1cb9fc22fcb7de9f09850d18ac8013c909dfd7d9
aveinash <avinashthakur.ytcreator@gmail.com>
2026-07-30 22:30:51 +0530

    add

---
+++ CHAPTER5- DICTIONARY & SETS/05_problem1.py
@@ -0,0 +1,9 @@
+#Write a program to create a dictionary of hindi words with values as their english translation.
+#Provide user with an optionto look it up
+words = {
+"madad": "help",
+"kursi": "chair",
+"billi": "cat"
+}
+word = input("enter the word you want meaning of: ")
+print(words[word])
