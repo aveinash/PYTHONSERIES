7dbfc390cbc9bfc0a70d98fac503c22d5b8d4437
aveinash <avinashthakur.ytcreator@gmail.com>
2026-08-01 15:27:48 +0530

    add

--- CHAPTER6- CONDITIONAL EXPRESSION/01_conditionals.py
+++ CHAPTER6- CONDITIONAL EXPRESSION/01_conditionals.py
@@ -1,7 +1,11 @@
 a = int(input("enter your age: "))
 
+# if else statement
 if(a>=18):
     print("you are above 18") #the gap before print is called as indentation
 
+
 else:
     print("you are below 18")
+
+print("end of program")
---
+++ CHAPTER6- CONDITIONAL EXPRESSION/02_if_elif_else_ladder.py
@@ -0,0 +1,13 @@
+a = int(input("enter your age "))
+
+#if elif else ladder
+if(a>18):
+    print("you are above 18")
+    print("good for you")
+elif(a==0):
+    print("please enter valid age 0 is not valid")
+elif(a<0):
+    print("not valid age")
+
+else:
+     print("you are below 18")
