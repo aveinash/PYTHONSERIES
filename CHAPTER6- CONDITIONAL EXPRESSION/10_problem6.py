a79d585a3239e22aa709b802d9ee0f5dcc18b3a3
aveinash <avinashthakur.ytcreator@gmail.com>
2026-08-01 18:38:38 +0530

    add

---
+++ CHAPTER6- CONDITIONAL EXPRESSION/10_problem6.py
@@ -0,0 +1,8 @@
+# write a program to find out whether a given post is talking about "python" or not 
+
+post = input("enter your message or post: ")
+
+if("python" in post):
+    print("yes the given post is talkin about python")
+else:
+    print("no")
