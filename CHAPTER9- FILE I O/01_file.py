bce3a60746a0e3e96f094d4a46ed93b39ffb6630
aveinash <avinashthakur.ytcreator@gmail.com>
2026-08-14 10:16:13 +0530

    add

---
+++ CHAPTER9- FILE I O/01_file.py
@@ -0,0 +1,4 @@
+f = open("CHAPTER9- FILE I O/file.txt")
+data = f.read()
+print(data)
+f.close()
---
+++ CHAPTER9- FILE I O/02_file_write.py
@@ -0,0 +1,7 @@
+st = "hello how are you"
+
+f = open("CHAPTER9- FILE I O/myfile.txt","w")
+
+f.write(st)
+
+f.close()
